6591879416dc5bb2d06105a2c6ea0d03689b0ea5
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-19 09:52:29 +0530

    Add:- Welcome Message

---
+++ src/com/Bridgelabs/LineComparison.java
@@ -0,0 +1,10 @@
+package com.Bridgelabs;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("*****Welcome to the line comparison computation program*****");
+		
+	}
+
+}
